a4843e9c107055673ddd1e5e523368af52964726
java <java>
2016-02-26 12:23:21 +0900

    mapper setting

--- src/test/java/com/hybrid/mapper/DeptMapperTest.java
+++ src/test/java/com/hybrid/mapper/DeptMapperTest.java
@@ -10,21 +10,34 @@ import com.hybrid.model.Dept;
 public class DeptMapperTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("spring/beans_db.xml");
+		GenericXmlApplicationContext ctx = 
+				new GenericXmlApplicationContext("spring/beans_db.xml");
+		DeptMapper mapper = ctx.getBean(DeptMapper.class);
 		
-//		SqlSessionTemplate sqlSession = ctx.getBean(SqlSessionTemplate.class);
-		SqlSessionTemplate sqlSession = (SqlSessionTemplate) ctx.getBean("sqlSessionTemplate");
-		
-		
-		DeptMapper deptMapper = sqlSession.getMapper(DeptMapper.class);
-		
-		List<Dept> depts = deptMapper.selectAll();
+		List<Dept> depts = mapper.selectAll();
 		for(Dept d : depts) {
 			System.out.println(d.getDeptno()+ " "+ d.getDname() + " " + d.getLoc());
 		}
 		
 		ctx.close();
+		
 	}
-
+	
+	void test1(){
+	// TODO Auto-generated method stub
+	GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("spring/beans_db.xml");
+	
+//		SqlSessionTemplate sqlSession = ctx.getBean(SqlSessionTemplate.class);
+	SqlSessionTemplate sqlSession = (SqlSessionTemplate) ctx.getBean("sqlSessionTemplate");
+	
+	
+	DeptMapper deptMapper = sqlSession.getMapper(DeptMapper.class);
+	
+	List<Dept> depts = deptMapper.selectAll();
+	for(Dept d : depts) {
+		System.out.println(d.getDeptno()+ " "+ d.getDname() + " " + d.getLoc());
+	}
+	
+	ctx.close();
+	}
 }
